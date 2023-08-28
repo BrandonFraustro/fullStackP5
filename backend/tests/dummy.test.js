3805f85462be247c6afa8dd4a298cb104d7588ca
Brandon Fraustro <Brandonfraustro20@gmail.com>
2023-08-27 18:23:01 -0600

    Auxiliary functions and testing, step2

--- backend/tests/dummy.test.js
+++ backend/tests/dummy.test.js
@@ -6,3 +6,29 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+  const listWithBlogs = [
+    {
+      _id: '5afhdhgetdghst',
+      title: 'Go To Statement',
+      author: 'Edsger W.',
+      url: 'www.edsgerw.com',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5afhdhgetdghst',
+      title: 'Go To Statement',
+      author: 'Edsger W.',
+      url: 'www.edsgerw.com',
+      likes: 5,
+      __v: 0
+    },
+  ]
+
+  test('when list has onle one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithBlogs)
+    expect(result).toBe(10)
+  })
+})
--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (lists) => {
+    const result = (sum, list) => {
+        return sum +  list.likes
+    }
+
+    return lists.reduce(result, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
