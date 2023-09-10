32b439911851ad9b806102c51f5bf5b517ed139c
Brandon Fraustro <Brandonfraustro20@gmail.com>
2023-09-10 17:15:47 -0600

    Bloglist step3, POST cantity and content

--- backend/tests/blog_api.test.js
+++ backend/tests/blog_api.test.js
@@ -24,6 +24,30 @@ test('id is defined', async () => {
   // Verificar si id está definido en al menos un objeto de la respuesta
   expect(response.body[0].id).toBeDefined();
 });
+
+test('insert a new blog', async () => {
+  const newBlog = {
+    title: 'test 1',
+    author: 'Fatima',
+    url: 'fatima.com',
+    likes: 15
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const size = response.body.length
+
+  console.log('length', size)
+  console.log('data', response.body);
+  
+  expect(response.body).toHaveLength(size);
+});
   
 
 afterAll(() => {
