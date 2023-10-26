9f46fffef69c7c6342a2cb88cbf6d77c64ef8d1f
Brandon Fraustro <Brandonfraustro20@gmail.com>
2023-10-25 21:00:44 -0600

    Fronten de la lista de blogs, paso 7

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,30 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title}
-  </div>  
-)
+import { useState } from 'react'
+import './Blog.css'
+
+const Blog = ({ blog }) => {
+  const [dataBlog, setDataBlog] = useState(false)
+
+  const hideWhenVisible = { display: dataBlog ? 'none' : '' }
+  const showWhenVisible = { display: dataBlog ? '' : 'none' }
+
+  return (
+    <div className="blog-content">
+      <div style={hideWhenVisible}>
+        {blog.title} {blog.author}
+        <button className='button-blog' onClick={() => setDataBlog(true)}>View</button>
+      </div>
+      <div style={showWhenVisible}>
+        <p>{blog.title}</p>
+        <p>{blog.url}</p>
+        <div>
+          {blog.likes}
+          <button className='button-blog' >Like</button>
+        </div>
+        <p>{blog.author}</p>
+        <button className='button-blog' onClick={() => setDataBlog(false)}>Hide</button>
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
